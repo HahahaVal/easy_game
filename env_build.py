117e90e4c2c7cb89d19f4bb88ba999a5e58b33ae
guojin <you@example.com>
2021-11-12 15:51:22 +0800

    构建环境脚本

--- env_build.py
+++ env_build.py
@@ -7,16 +7,18 @@ env = root_path+"/env"
 
 subprocess.call("mkdir -p "+env, shell = True)
 
-def main():
-    python()
-    rely()
-    cmake()
-    lua()
+def yum():
+    #centos7替换yum源
+    subprocess.call("wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo", shell = True)
 
 def python():
+    #安装python3
+    subprocess.call("sudo yum install python3 -y", shell = True)
+    subprocess.call("pip3 install --upgrade setuptools", shell = True)
     subprocess.call("pip3 install virtualenv", shell = True)
 
 def rely():
+    #安装gcc等编译工具
     subprocess.call("yum groupinstall 'Development Tools'", shell = True)
     subprocess.call("yum install -y gcc gcc-c++ make automake", shell = True)
     subprocess.call("yum -y install autoconf", shell = True)
@@ -24,6 +26,7 @@ def rely():
     subprocess.call("yum -y install openssl-devel", shell = True)
 
 def cmake():
+    #安装cmake3.22
     cmd1 = "cd "+env
     cmd2 = "wget https://github.com/Kitware/CMake/releases/download/v3.22.0-rc2/cmake-3.22.0-rc2.tar.gz"
     cmd3 = "tar xzvf cmake-3.22.0-rc2.tar.gz"
@@ -35,6 +38,7 @@ def cmake():
     subprocess.call(cmd, shell = True)
 
 def lua():
+    #安装lua5.4
     cmd1 = "cd "+env
     cmd2 = "wget http://www.lua.org/ftp/lua-5.4.2.tar.gz"
     cmd3 = "tar zxvf lua-5.4.2.tar.gz"
@@ -43,6 +47,30 @@ def lua():
     cmd6 = "make install"
     cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3 + "&&" + cmd4 + "&&" + cmd5 + "&&" + cmd6
     subprocess.call(cmd, shell = True)
-    
+
+def mongo():
+    #安装mongo4.2
+    repo = r"""
+[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc
+"""
+    cmd1 = "echo -e " +"\'"+ repo +"\'"+ " > /etc/yum.repos.d/mongodb-org-4.0.repo"
+    cmd2 = "yum install -y mongodb-org"
+    cmd3 = "systemctl start mongod"
+    cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3
+    subprocess.call(cmd, shell = True)
+
+def main():
+    yum()
+    python()
+    rely()
+    cmake()
+    lua()
+    mongo()
+
 if __name__ == "__main__":
     main()
