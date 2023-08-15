81c3ba6204458b952e59e533078340926a5ff23a
guojin <1018485371@qq.com>
2023-08-15 23:20:10 +0800

    环境安装脚本优化

--- env_build.py
+++ env_build.py
@@ -5,67 +5,92 @@ import subprocess
 root_path = os.path.expandvars('$HOME')
 env = root_path+"/env"
 
-subprocess.call("mkdir -p "+env, shell = True)
+subprocess.call("mkdir -p "+env, shell=True)
+
 
 def yum():
-    #centos8替换yum源
-    subprocess.call("cd /etc/yum.repos.d/", shell = True)
-    subprocess.call("rm -rf *", shell = True)
-    subprocess.call("wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo", shell = True)
-    subprocess.call("yum clean all && yum makecache", shell = True)
- 
+    # centos8替换yum源
+    cmds = [
+        "cd /etc/yum.repos.d/",
+        "rm -rf *",
+        "wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo",
+        "yum clean all && yum makecache",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
+
+
 def python():
-    #安装python3
-    subprocess.call("sudo yum install python3 -y", shell = True)
-    subprocess.call("pip3 install --upgrade setuptools", shell = True)
-    subprocess.call("pip3 install virtualenv", shell = True)
+    # 安装python3
+    cmds = [
+        "sudo yum install python3 -y",
+        "pip3 install --upgrade setuptools",
+        "pip3 install virtualenv",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
+
 
 def rely():
-    #安装gcc等编译工具
-    subprocess.call("yum groupinstall 'Development Tools'", shell = True)
-    subprocess.call("yum install -y gcc gcc-c++ make automake", shell = True)
-    subprocess.call("yum -y install autoconf", shell = True)
-    subprocess.call("yum -y install readline-devel", shell = True)
-    subprocess.call("yum -y install openssl-devel", shell = True)
+    # 安装gcc等编译工具
+    cmds = [
+        "yum groupinstall 'Development Tools'",
+        "yum install -y gcc gcc-c++ make automake",
+        "yum -y install autoconf",
+        "yum -y install readline-devel",
+        "yum -y install openssl-devel",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
+
 
 def cmake():
-    #安装cmake3.22
-    cmd1 = "cd "+env
-    cmd2 = "wget https://github.com/Kitware/CMake/releases/download/v3.22.0-rc2/cmake-3.22.0-rc2.tar.gz"
-    cmd3 = "tar xzvf cmake-3.22.0-rc2.tar.gz"
-    cmd4 = "cd cmake-3.22.0-rc2/"
-    cmd5 = "./configure"
-    cmd6 = "make"
-    cmd7 = "make install"
-    cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3 + "&&" + cmd4 + "&&" + cmd5 + "&&" + cmd6 + "&&" + cmd7
-    subprocess.call(cmd, shell = True)
+    # 安装cmake3.22
+    cmds = [
+        "cd "+env,
+        "wget https://github.com/Kitware/CMake/releases/download/v3.22.0-rc2/cmake-3.22.0-rc2.tar.gz",
+        "tar xzvf cmake-3.22.0-rc2.tar.gz",
+        "cd cmake-3.22.0-rc2/",
+        "./configure",
+        "make",
+        "make install",
+
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
+
 
 def lua():
-    #安装lua5.4
-    cmd1 = "cd "+env
-    cmd2 = "wget http://www.lua.org/ftp/lua-5.4.2.tar.gz"
-    cmd3 = "tar zxvf lua-5.4.2.tar.gz"
-    cmd4 = "cd lua-5.4.2"
-    cmd5 = "make linux"
-    cmd6 = "make install"
-    cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3 + "&&" + cmd4 + "&&" + cmd5 + "&&" + cmd6
-    subprocess.call(cmd, shell = True)
+    # 安装lua5.4
+    cmds = [
+        "cd "+env,
+        "wget http://www.lua.org/ftp/lua-5.4.2.tar.gz",
+        "tar zxvf lua-5.4.2.tar.gz",
+        "cd lua-5.4.2",
+        "make linux",
+        "make install",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
+
 
 def go():
-    #安装go1.18.1
-    cmd1 = "cd "+env
-    cmd2 = "wget https://go.dev/dl/go1.18.1.linux-amd64.tar.gz"
-    cmd3 = "tar -C /usr/local/ -zxvf go1.18.1.linux-amd64.tar.gz"
-    cmd4 = "echo 'export GOROOT=/usr/local/go' | sudo tee -a /etc/profile"
-    cmd5 = "echo 'export PATH=$PATH:$GOROOT/bin' | sudo tee -a /etc/profile"
-    cmd6 = "echo 'export GOPROXY=https://goproxy.io' | sudo tee -a /etc/profile"
-    cmd7 = "source /etc/profile"
+    # 安装go1.18.1
+    cmds = [
+        "cd "+env,
+        "wget https://go.dev/dl/go1.18.1.linux-amd64.tar.gz",
+        "tar -C /usr/local/ -zxvf go1.18.1.linux-amd64.tar.gz",
+        "echo 'export GOROOT=/usr/local/go' | sudo tee -a /etc/profile",
+        "echo 'export PATH=$PATH:$GOROOT/bin' | sudo tee -a /etc/profile",
+        "echo 'export GOPROXY=https://goproxy.io' | sudo tee -a /etc/profile",
+        "source /etc/profile",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
 
-    cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3 + "&&" + cmd4 + "&&" + cmd5 + "&&" + cmd6 + "&&" + cmd7
-    subprocess.call(cmd, shell = True)
 
 def mongo():
-    #安装mongo4.2
+    # 安装mongo4.2
     repo = r"""
 [mongodb-org-4.2]
 name=MongoDB Repository
@@ -74,14 +99,17 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc
 """
-    cmd1 = "echo -e " +"\'"+ repo +"\'"+ " > /etc/yum.repos.d/mongodb-org-4.0.repo"
-    cmd2 = "yum install -y mongodb-org"
-    cmd3 = "systemctl start mongod"
-    cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3
-    subprocess.call(cmd, shell = True)
+    cmds = [
+        "echo -e "+"\'"+repo+"\'"+" > /etc/yum.repos.d/mongodb-org-4.0.repo",
+        "yum install -y mongodb-org",
+        "systemctl start mongod",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
+
 
 def etcd1():
-    #安装etcd3.5
+    # 安装etcd3.5
     system = r"""
 [Unit]
 Description=Etcd Server
@@ -108,32 +136,29 @@ ETCD_INITIAL_CLUSTER="etcd1=http://127.0.0.1:12380,etcd2=http://127.0.0.1:22380,
 ETCD_INITIAL_CLUSTER_STATE="new"
 ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster"
 """
-    cmd1 = "cd "+env
-    cmd2 = "wget https://github.com/etcd-io/etcd/releases/download/v3.5.4/etcd-v3.5.4-linux-amd64.tar.gz"
-    cmd3 = "tar xzvf etcd-v3.5.4-linux-amd64.tar.gz"
-    cmd4 = "mv etcd-v3.5.4-linux-amd64 etcd"
-    cmd5 = "cd etcd"
-    cmd6 = "cp etcd etcdctl /usr/bin"
-    cmd7 = "mkdir -p /opt/etcd/"
-    cmd8 = "mkdir -p /etc/etcd"
-    cmd9 = "echo -e " +"\'"+ system +"\'"+ " > /usr/lib/systemd/system/etcd1.service"
-    cmd10 = "echo -e " +"\'"+ config +"\'"+ " > /etc/etcd/etcd1.conf"
-    cmd11 = "systemctl daemon-reload"
-    cmd12 = "systemctl enable etcd1"
-    cmd13 = "systemctl restart etcd1"
-    cmd14 = "etcdctl --endpoints=127.0.0.1:12379 user add root <<EOF
-123456
-123456
-EOF"
-    cmd15 = "etcdctl --endpoints=127.0.0.1:12379 auth enable"
+    cmds = [
+        "cd "+env,
+        "wget https://github.com/etcd-io/etcd/releases/download/v3.5.4/etcd-v3.5.4-linux-amd64.tar.gz",
+        "tar xzvf etcd-v3.5.4-linux-amd64.tar.gz",
+        "mv etcd-v3.5.4-linux-amd64 etcd",
+        "cd etcd",
+        "cp etcd etcdctl /usr/bin",
+        "mkdir -p /opt/etcd/",
+        "mkdir -p /etc/etcd",
+        "echo -e "+"\'"+system+"\'"+" > /usr/lib/systemd/system/etcd1.service",
+        "echo -e "+"\'"+config+"\'"+" > /etc/etcd/etcd1.conf",
+        "systemctl daemon-reload",
+        "systemctl enable etcd1",
+        "systemctl restart etcd1",
+        "etcdctl --endpoints=127.0.0.1:12379 user add root <<EOF\n123456\n123456\nEOF",
+        "etcdctl --endpoints=127.0.0.1:12379 auth enable",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
 
-    cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3 + "&&" + cmd4 + "&&" + cmd5 + "&&" + cmd6 \
-            + "&&" + cmd7 + "&&" + cmd8 + "&&" + cmd9 + "&&" + cmd10 + "&&" + cmd11 + "&&" + cmd12  \
-            + "&&" + cmd13 + "&&" + cmd14 + "&&" + cmd15
-    subprocess.call(cmd, shell = True)
 
 def etcd2():
-    #安装etcd3.5
+    # 安装etcd3.5
     system = r"""
 [Unit]
 Description=Etcd Server
@@ -160,17 +185,19 @@ ETCD_INITIAL_CLUSTER="etcd1=http://127.0.0.1:12380,etcd2=http://127.0.0.1:22380,
 ETCD_INITIAL_CLUSTER_STATE="new"
 ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster"
 """
-    cmd1 = "echo -e " +"\'"+ system +"\'"+ " > /usr/lib/systemd/system/etcd2.service"
-    cmd2 = "echo -e " +"\'"+ config +"\'"+ " > /etc/etcd/etcd2.conf"
-    cmd3 = "systemctl daemon-reload"
-    cmd4 = "systemctl enable etcd2"
-    cmd5 = "systemctl restart etcd2"
+    cmds = [
+        "echo -e "+"\'"+system+"\'"+" > /usr/lib/systemd/system/etcd2.service",
+        "echo -e "+"\'"+config+"\'"+" > /etc/etcd/etcd2.conf",
+        "systemctl daemon-reload",
+        "systemctl enable etcd2",
+        "systemctl restart etcd2",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
 
-    cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3 + "&&" + cmd4 + "&&" + cmd5
-    subprocess.call(cmd, shell = True)
 
 def etcd3():
-    #安装etcd3.5
+    # 安装etcd3.5
     system = r"""
 [Unit]
 Description=Etcd Server
@@ -197,17 +224,19 @@ ETCD_INITIAL_CLUSTER="etcd1=http://127.0.0.1:12380,etcd2=http://127.0.0.1:22380,
 ETCD_INITIAL_CLUSTER_STATE="new"
 ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster"
 """
-    cmd1 = "echo -e " +"\'"+ system +"\'"+ " > /usr/lib/systemd/system/etcd3.service"
-    cmd2 = "echo -e " +"\'"+ config +"\'"+ " > /etc/etcd/etcd3.conf"
-    cmd3 = "systemctl daemon-reload"
-    cmd4 = "systemctl enable etcd3"
-    cmd5 = "systemctl restart etcd3"
+    cmds = [
+        "echo -e "+"\'"+system+"\'"+" > /usr/lib/systemd/system/etcd3.service",
+        "echo -e "+"\'"+config+"\'"+" > /etc/etcd/etcd3.conf",
+        "systemctl daemon-reload",
+        "systemctl enable etcd3",
+        "systemctl restart etcd3",
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
 
-    cmd = cmd1 + "&&" + cmd2 + "&&" + cmd3 + "&&" + cmd4 + "&&" + cmd5
-    subprocess.call(cmd, shell = True)
 
 def nginx():
-    #配置nginx
+    # 配置nginx
     config = r"""
 upstream etcd{
     server  127.0.0.1:12379;
@@ -227,11 +256,13 @@ server {
     }
 }
 """
-    cmd1 = "echo -e " +"\'"+ config +"\'"+ " > /etc/nginx/conf.d/etcd.conf"
-    cmd2 = "nginx"
+    cmds = [
+        "echo -e "+"\'"+config+"\'"+" > /etc/nginx/conf.d/etcd.conf",
+        "nginx"
+    ]
+    cmd = "&&".join(cmds)
+    subprocess.call(cmd, shell=True)
 
-    cmd = cmd1 + "&&" + cmd2
-    subprocess.call(cmd, shell = True)
 
 def main():
     yum()
@@ -245,6 +276,7 @@ def main():
     etcd2()
     etcd3()
     nginx()
+
 
 if __name__ == "__main__":
     main()
